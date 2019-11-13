40afac4bdcef63fa582157a161cce185bb9ce7d8
AliKheir <akheirali@gmail.com>
2019-11-14 00:05:15 +0300

    created admin and setup superuser credentials

--- insta/models.py
+++ insta/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey('auth.user',on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
     image = models.ImageField(blank=True, null=True)
-    caption = models.TextFieldField()
+    caption = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
