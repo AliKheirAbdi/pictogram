2a8e522f7dfa52cba2c848cdb8e54b00b80f2f32
AliKheir <akheirali@gmail.com>
2019-11-13 23:19:43 +0300

    created image model in models.py

--- insta/models.py
+++ insta/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.user',on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
