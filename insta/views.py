04ea863d821074014614b7a5521a76ed16088f49
AliKheir <akheirali@gmail.com>
2019-11-15 23:07:42 +0300

    created a forms file with PostForm class

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import(
+    ListView
+)
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    template_name = "insta/home.html"
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
--- instaclone/settings.py
+++ instaclone/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'insta'
+    'insta',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
