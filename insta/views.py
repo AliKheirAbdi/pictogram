77365f73b88a31df0621d29fe630287bc8854d31
AliKheir <akheirali@gmail.com>
2019-11-16 12:26:46 +0300

    created the PostDetailView in views.py

--- insta/views.py
+++ insta/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
+from django.utils import timezone
 from django.views.generic import(
-    ListView
+    ListView,
+    CreateView,
+    DetailView,
 )
+from datetime import datetime
+from .models import Post
 
 # Create your views here.
 
@@ -14,9 +20,22 @@ class PostListView(ListView):
 
 
 class PostCreateView(CreateView):
-    template = 'insta/post_create.html'
+    template_name = 'insta/post_create.html'
     form_class = PostForm
     queryset = Post.objects.all()
+    #to send users back to homepage after creating a post
+    success_url = '/'
 
-    def form_invalid(self, form):
+    def form_valid(self, form):
         print(form.cleaned_data)
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostDetailView(DetailView):
+    template_name = 'insta/post_detail.html'
+    queryset = Post.objects.all().filter(created_date__lte=timezone.now()).order_by('-created_date')
+
+    def get_object(self):
+        id_ = self.kwargs.get('id')
+        return get_object_or_404(Post, id=id_)
